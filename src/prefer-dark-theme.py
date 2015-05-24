0eebe827bd86bae5f7f8cdfb03470ae56f967239
Niko Lowry <nikolowry@gmail.com>
2015-05-24 01:50:26 -0400

    First Commit

---
+++ src/prefer-dark-theme.py
@@ -0,0 +1,18 @@
+from gi.repository import GObject, Gedit, Gtk
+
+class PreferDarkPlugin(GObject.Object, Gedit.WindowActivatable):
+    __gtype_name__ = "PreferDarkPlugin"
+
+    window = GObject.property(type=Gedit.Window)
+
+    def __init__(self):
+        GObject.Object.__init__(self)
+
+    def do_activate(self):
+        Gtk.Settings.get_default().set_property("gtk-application-prefer-dark-theme", True)
+
+    def do_deactivate(self):
+        Gtk.Settings.get_default().set_property("gtk-application-prefer-dark-theme", False)
+
+    def do_update_state(self):
+        pass
